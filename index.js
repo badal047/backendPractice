b7509d4902373d3156b99a9a23ac53cf27b7fb4d
badal047 <srikrishnabadal1008@gmail.com>
2024-01-27 21:38:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/twitter', (req, res) => {
+    res.send('Hello twitter!')
+})
+app.get('/login', (req, res) => {
+    res.send('<h1m>Ye login page hai</h1m>')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
